bc129ee27f8e5347a9477d8459265e7d6fd8af01
Juan Esteban Gil Martínez <gilj06@hotmail.com>
2024-06-24 14:20:27 +0200

    Création de la vue global des tous les habitats, et correction de la taille des textes

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,7 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/accueil", "Accueil", "/pages/home.html"),
     new Route("/habitats", "Les Habitats", "/pages/vue_global_habitats.html"),
 ];
 
