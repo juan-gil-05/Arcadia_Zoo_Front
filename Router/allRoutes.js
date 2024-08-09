08b775462dcd99a463404732f61ccc4f2d0a63b9
Juan Esteban Gil Martínez <gilj06@hotmail.com>
2024-08-09 16:15:58 +0200

    Creation du big titre de la page habitat-jungle

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,8 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/accueil", "Accueil", "/pages/home.html"),
     new Route("/habitats", "Les Habitats", "/pages/vue_global_habitats.html"),
-    new Route("/habitat_savane", "La Savane", "/pages/Habitats/habitat_savane.html"),
+    new Route("/habitat_Savane", "La Savane", "/pages/Habitats/habitat_savane.html"),
+    new Route("/habitat_Jungle", "La Jungle", "/pages/Habitats/habitat_jungle.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
--- Scripts/script.js
+++ Scripts/script.js
@@ -1,4 +1,4 @@
 // fonction pour bouger la description de l'habitat quand on clique sur le menu burger
 document.querySelector(".navbar-toggler").addEventListener('click', function () {
-    document.querySelector(".savane-description").classList.toggle("move-left")
+    document.querySelector(".description").classList.toggle("move-left")
 })
