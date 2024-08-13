5bbc2c8515db8e8dbc70a3f3d22ea8ca9ceed958
Juan Esteban Gil Martínez <gilj06@hotmail.com>
2024-08-13 15:34:51 +0200

    Creation de la page habitat-marais et correction de la taille des images pour amélioration de performance

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,7 @@ export const allRoutes = [
     new Route("/habitats", "Les Habitats", "/pages/vue_global_habitats.html"),
     new Route("/habitat_Savane", "La Savane", "/pages/Habitats/habitat_savane.html"),
     new Route("/habitat_Jungle", "La Jungle", "/pages/Habitats/habitat_jungle.html"),
+    new Route("/habitat_Marais", "Habitat Marais", "/pages/Habitats/habitat_marais.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
