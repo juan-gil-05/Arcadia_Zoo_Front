0cc49bd8174e4d94e55fe86f56f7312f5880bc39
Juan Esteban Gil Martínez <gilj06@hotmail.com>
2024-08-15 14:40:28 +0200

    Création de la page habitat-forêt

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,9 +4,10 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/accueil", "Accueil", "/pages/home.html"),
     new Route("/habitats", "Les Habitats", "/pages/vue_global_habitats.html"),
-    new Route("/habitat_Savane", "La Savane", "/pages/Habitats/habitat_savane.html"),
-    new Route("/habitat_Jungle", "La Jungle", "/pages/Habitats/habitat_jungle.html"),
-    new Route("/habitat_Marais", "Habitat Marais", "/pages/Habitats/habitat_marais.html"),
+    new Route("/habitats-savane", "La Savane", "/pages/Habitats/habitat_savane.html"),
+    new Route("/habitats-jungle", "La Jungle", "/pages/Habitats/habitat_jungle.html"),
+    new Route("/habitats-marais", "Habitat Marais", "/pages/Habitats/habitat_marais.html"),
+    new Route("/habitats-foret", "Habitat Forêt", "/pages/Habitats/habitat_foret.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
