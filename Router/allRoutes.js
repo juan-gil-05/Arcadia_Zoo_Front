2520363c273389a19ea65944ab2c0423848eece3
Juan Esteban Gil Martínez <gilj06@hotmail.com>
2024-08-19 15:35:06 +0200

    correction des dossier des images, et création de la page vue_global_services

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,14 +2,18 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
+    //page d'accueil
     new Route("/Index.html", "Accueil", "/pages/home.html"),
     new Route("/accueil", "Accueil", "/pages/home.html"),
+    //pages habitats
     new Route("/habitats", "Les Habitats", "/pages/vue_global_habitats.html"),
     new Route("/habitats-savane", "La Savane", "/pages/Habitats/habitat_savane.html"),
     new Route("/habitats-jungle", "La Jungle", "/pages/Habitats/habitat_jungle.html"),
     new Route("/habitats-marais", "Habitat Marais", "/pages/Habitats/habitat_marais.html"),
     new Route("/habitats-foret", "Habitat Forêt", "/pages/Habitats/habitat_foret.html"),
     new Route("/habitats-aquarium", "Habitat Aquarium", "/pages/Habitats/habitat_aquarium.html"),
+    //page services
+    new Route("/services", "Nos Services", "/pages/vue_global_services.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
